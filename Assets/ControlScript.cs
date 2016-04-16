884347af91cf26b872d3eb8cd9bc6a4e7a5d7673
stromling <stromling@gmail.com>
2016-04-17 01:43:47 +0200

    Ship broken, only rotates

--- Assets/ControlScript.cs
+++ Assets/ControlScript.cs
@@ -3,18 +3,27 @@ using System.Collections;
 
 public class ControlScript : MonoBehaviour {
 
+	const float ROTATESPEED = 8f;
+
+	float RotationSpeed = 0f;
+
 	// Use this for initialization
 	void Start() {
-	
+//		Vector2 com = GetComponentInParent<Rigidbody2D>().centerOfMass;
+//		com.y -= 0.1f;
+//		GetComponentInParent<Rigidbody2D>().centerOfMass = com;
 	}
 
 	// Update is called once per frame
-	void LateUpdate() {
-		Vector3 pos = GetComponentInParent<Transform>().position;
-		if (Input.GetKey(KeyCode.W)) pos.y += 0.05f;
-		if (Input.GetKey(KeyCode.S)) pos.y -= 0.05f;
-		if (Input.GetKey(KeyCode.D)) pos.x += 0.05f;
-		if (Input.GetKey(KeyCode.A)) pos.x -= 0.05f;
-		GetComponentInParent<Transform>().position = pos;
+	void FixedUpdate() {
+		Rigidbody2D rb = GetComponentInParent<Rigidbody2D>();
+
+		if (Input.GetKey(KeyCode.D)) RotationSpeed += -ROTATESPEED;
+		if (Input.GetKey(KeyCode.A)) RotationSpeed += ROTATESPEED;
+		rb.MoveRotation(rb.rotation + RotationSpeed * Time.fixedDeltaTime);
+
+		//t.forward
+		//if (Input.GetKey(KeyCode.W)) pos.y += 0.05f;
+		//if (Input.GetKey(KeyCode.S)) pos.y -= 0.05f;
 	}
 }
