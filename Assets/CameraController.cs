c5916d9728d10c242068a8d9b9d3ef824a27d8a8
stromling <stromling@gmail.com>
2016-04-17 02:44:37 +0200

    KAMERA KONTROLLER

---
+++ Assets/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	const float ZOOMSPEED = 0.5f;
+
+	Camera cam;
+
+	Transform pst;
+	Transform t;
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera>();
+
+		pst = GameObject.Find("PlayerSprite").GetComponent<Transform>();
+		t = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		t.position = new Vector3(pst.position.x, pst.position.y, t.position.z);
+
+		if (Input.mouseScrollDelta.y != 0) cam.orthographicSize -= Input.mouseScrollDelta.y*ZOOMSPEED;
+
+		GameObject.Find("Text").GetComponent<UnityEngine.UI.Text>().text = "SCROLLDELTA: " + Input.mouseScrollDelta.ToString();
+	}
+}
