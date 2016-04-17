9cf78cbe6d10a44730dce1cf7644d31c5f32f24a
Christoffer Karlsson <christoffer_karlsson@live.se>
2016-04-17 02:44:20 +0200

    parralax

---
+++ Assets/StarfieldScroll.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarfieldScroll : MonoBehaviour {
+	
+    float parralax = 2f;
+	// Update is called once per frame
+	void Update () {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        Camera cam = FindObjectOfType<Camera>();
+        Material mat = mr.material;
+        Vector2 offset = mat.mainTextureOffset;
+        offset.x = transform.position.x / transform.localScale.x / parralax;
+        offset.y = transform.position.y / transform.localScale.y / parralax;
+
+        mat.mainTextureOffset = offset;
+        this.transform.rotation = cam.transform.rotation;
+	}
+}
